d88271f932c36d701647f481f5d69f7c448a0ce1
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2017-04-20 14:10:23 -0500

    Primer sincronización
    Primer sincronización

---
+++ MySqlExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlUtilerias
+{
+    public class MySqlExceptionHandler
+    {
+        public static TipoExcepcion obtenerTipoExcepcion(MySqlException e)
+        {
+            switch (e.Number)
+            {
+
+                default:
+                    return TipoExcepcion.ErrorEnServidor;
+            }
+        }
+    }
+}
---
+++ TipoExcepcion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlUtilerias
+{
+    public enum TipoExcepcion
+    {
+        ErrorConexionServidor,
+        ErrorDesconocido,
+        ErrorEnServidor,
+        ErrorSintaxisSQL
+    }
+}
